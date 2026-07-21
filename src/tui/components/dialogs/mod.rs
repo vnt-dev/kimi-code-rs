4e2a6c920fbf8281a01d3f6d5e0300ef92807368
lbl <1791778603@qq.com>
2026-07-22 05:15:40 +0800

    migrate: port start permission prompt to Rust

--- src/tui/components/dialogs/mod.rs
+++ src/tui/components/dialogs/mod.rs
@@ -4,6 +4,7 @@ pub mod effort_selector;
 pub mod permission_selector;
 pub mod platform_selector;
 pub mod settings_selector;
+pub mod start_permission_prompt;
 pub mod theme_selector;
 pub mod update_preference_selector;
 
@@ -15,5 +16,8 @@ pub use effort_selector::EffortSelectorComponent;
 pub use permission_selector::PermissionSelectorComponent;
 pub use platform_selector::PlatformSelectorComponent;
 pub use settings_selector::{SettingsSelection, SettingsSelectorComponent};
+pub use start_permission_prompt::{
+    StartPermissionOption, StartPermissionPromptComponent, StartPermissionPromptOptions,
+};
 pub use theme_selector::ThemeSelectorComponent;
 pub use update_preference_selector::UpdatePreferenceSelectorComponent;
